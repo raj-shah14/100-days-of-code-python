a967d4051c8a1bc6c98a59e5e79d6e525aa29805
raj-shah14 <rajpareshshah14@gmail.com>
2024-09-22 11:40:57 -0700

    Adding changes for Day 2

--- day02/main.py
+++ day02/main.py
@@ -4,6 +4,7 @@ bill = float(input("What is the total bill amount?\n$:"))
 tip = int(input("How much tip would you like to give?\nPercent:"))
 split = int(input("How many people to split the bill?\nPeople:"))
 
-total = ("{:.2f}".format((((bill * (tip / 100)) + bill) / split)))
+bill_with_tip = bill + (tip * bill)/100
+share_for_each_person = bill_with_tip / split
 
-print(f"Each person should pay: ${total}")
+print(f"Each person has to pay {share_for_each_person}")
