196c078e3c2797baedc1f0055b425dc68719dcd3
raj-shah14 <rajpareshshah14@gmail.com>
2024-09-22 20:56:07 -0700

    Encode and decode message

--- day08/main.py
+++ day08/main.py
@@ -1,33 +1,26 @@
 from art import logo
 
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'] #'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-def caesar(start_text, shift_amount, cipher_direction):
-  end_text = ""
-  if cipher_direction == "decode":
-    shift_amount *= -1
-  for char in start_text:
-    if char.isalpha():
-      position = alphabet.index(char)
-      new_position = position + shift_amount
-      end_text += alphabet[new_position]
-    else:
-      end_text += char
-  print(f"Here's the {cipher_direction}d result: {end_text}\n")
+text = input("Select whether you want to encrypt or decrypt\n 1 --> Encode \n 2 --> Decode \n")
+message = input("Enter a message\n")
+shift = input("Shift the text by Number \n")
 
-run = True
-while run:
-  print(logo)
-  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-  text = input("Type your message:\n").lower()
-  shift = int(input("Type the shift number:\n"))
+if text == "2":
+  shift = int(shift) * -1
 
-  if shift > 26:
-    shift = shift % shift == 0
-
-  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
-  choice = input("Do you want to run this program again?\nType 'yes' or 'no': ")
-  if choice == 'no':
-    run = False
-    print("Goodbye.")
+res = ''
+for c in message:
+    if c.isalpha():
+      new_ord = ord(c) + int(shift) 
+      if new_ord > ord('z'):    # Encode
+        new_ord = (new_ord - ord('a')) % 26 + ord('a')
+      
+      if new_ord < ord('a'):    #Decode
+        new_ord = (new_ord - ord('a')) % 26 + ord('a')
+        
+      res += chr(new_ord)
+    if c == " ":
+       res += c
+print(res)
