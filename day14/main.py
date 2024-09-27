93bc91035f33c2b3b7e172f2a945af6085bfa81e
raj-shah14 <rajpareshshah14@gmail.com>
2024-09-26 21:14:51 -0700

    Updating higher lower game

--- day14/main.py
+++ day14/main.py
@@ -1,52 +1,30 @@
 from art import logo, vs
 from game_data import data
-from replit import clear
 import random
 
+print(logo)
 
-def profile():
-  random_number = random.randint(0, 49)
-  name = data[random_number]['name']
-  follower_count = data[random_number]['follower_count']
-  description = data[random_number]['description']
-  country = data[random_number]['country']
-  compare = (f"{name}, a {description}, from {country}.")
-  return compare, follower_count
+def get_profile(id):
+    profile = data[id]
+    return profile
 
-def higher_lower():
-  profile_a, followers_a = profile()
-  profile_b, followers_b = profile()
-  if followers_a == followers_b:
-    profile_b, followers_b = profile()
-  score = 0
-  correct = True
+score = 0
+correct = True
 
-  while correct:
-    clear()
-    print(logo)
-    if score > 0:
-      print(f"Correct! Your current score is: {score}.")
-    # print (followers_a, followers_b)
-    print(f"Compare A: {profile_a}")
-    print(vs)
-    print(f"Compare B: {profile_b}")
+while correct:
+  print(f"Score: {score}")
+  a, b = random.sample(range(0, len(data)-1), 2)
+  profile_a, profile_b  = get_profile(a), get_profile(b)
+  print(f"Option A: {profile_a["name"]}, {profile_a["description"]}, {profile_a["country"]}")
+  a_followers, b_followers = profile_a["follower_count"], profile_b["follower_count"]
+  correct_answer = "A" if (a_followers > b_followers) else "B"
+  print(vs)
+  print(f"Option B: {profile_b["name"]}, {profile_b["description"]}, {profile_b["country"]}")
 
-    a_or_b = input("\nWho do you think has more followers? Type 'a', or 'b': ").lower()
-    if a_or_b == 'a' and followers_a >= followers_b:
+  user_input = input("Who has more followers on Instagram?\n")
+  if user_input == correct_answer:
       score += 1
-      profile_b, followers_b = profile()
-    elif a_or_b == 'b' and followers_a <= followers_b:
-      score += 1
-      profile_a, followers_a = profile()
-    elif a_or_b == 'a' and followers_a < followers_b:
-      print(f"\nIncorrect, your final score is {score}.")
+  else:
+      print(f"Your final Score: {score}")
+      score = 0
       correct = False
-    elif a_or_b == 'b' and followers_a > followers_b:
-      print(f"\nIncorrect, your final score is {score}.")
-      correct = False
-  play_again = input("\nDo you want to play again? Type 'y' or 'n': ")
-  if play_again == 'y':
-    higher_lower()
-  elif play_again == 'n':
-    print("Thanks for playing!")
-higher_lower()
