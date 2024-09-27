31c3475193f3198d4596a7adc606d32bab609f6f
raj-shah14 <rajpareshshah14@gmail.com>
2024-09-26 23:26:28 -0700

    Building Coffee machine

--- day15/main.py
+++ day15/main.py
@@ -34,74 +34,83 @@ money = {
     "value": 0,
 }
 
+def get_report():
+    print(f" Water:{resources["water"]}ml \n Milk: {resources['milk']}ml \n Coffee: {resources['coffee']}g \n Money: ${money['value']}")
 
-def report():
-    print(f"Water: {resources['water']}ml")
-    print(f"Milk: {resources['milk']}ml")
-    print(f"Coffee: {resources['coffee']}g")
-    print(f"Money: ${money['value']}")
+def calculate_total(q, p, n, d):
+    q *= 0.25
+    p *= .10
+    n *= .05
+    d *= .01 
+    return q + p + n + d
 
+def validate_cost(coffee, amt):
+    if amt < coffee['cost']:
+        print(f"Insufficient Funds. Please add ${round(coffee["cost"] - amt, 2)} and retry. Returning your change")
+    return 
+    
+def check_resources(user_input, coffee, resources):
+    # Check water
+    if resources["water"] < coffee['ingredients']['water']:
+        print("Sorry not Enough Water. Money Refunded")
+    
+    # Check Coffee
+    if resources["coffee"] < coffee['ingredients']['coffee']:
+        print("Sorry not Enough Coffee. Money Refunded")
+        
+    # Check milk
+    if user_input != 'espresso':
+        if resources["milk"] < coffee['ingredients']['milk']:
+            print("Sorry not Enough Milk. Money Refunded")
 
-def check_resources(coffee_choice):
-    enough = True
-    if MENU[coffee_choice]['ingredients']['water'] > resources['water']:
-        print(f"There isn't enough water for a {coffee_choice}.")
-        enough = False
-    if MENU[coffee_choice]['ingredients']['coffee'] > resources['coffee']:
-        print(f"There isn't enough coffee for a {coffee_choice}.")
-        enough = False
-    if coffee_choice != 'espresso':
-        if MENU[coffee_choice]['ingredients']['milk'] > resources['milk']:
-            print(f"There isn't enough milk for a {coffee_choice}.")
-            enough = False
-    return enough
+def request_money():
+    print("Please insert Coins")
+    quarters = int(input("Quarters: "))
+    dimes = int(input("Dimes: "))
+    nickles = int(input("Nickles: "))
+    pennies = int(input("Pennies: "))
+    amt_paid = calculate_total(quarters, pennies, nickles, dimes)
+    print(f"Total amount Paid: ${round(amt_paid, 2)}")
+    return amt_paid
 
+def dispense_coffee(user_input, coffee, resources):
+    # update reosurces
+    resources["water"] -= coffee['ingredients']['water']
+    if user_input != 'espresso':
+        resources["milk"] -= coffee['ingredients']['milk']
+    resources["coffee"] -= coffee['ingredients']['coffee']
+    print("Enjoy your Coffee! ☕")
 
-def process_coins(quarter, dime, nickle, penny):
-    quarter *= 0.25
-    dime *= 0.10
-    nickle *= 0.05
-    penny *= 0.01
-    total = quarter + dime + nickle + penny
-    return total
+def update_profit(coffee):
+    money['value'] += coffee['cost']
 
-
-def deduct_resources(coffee_choice):
-    resources['water'] -= MENU[coffee_choice]['ingredients']['water']
-    resources['coffee'] -= MENU[coffee_choice]['ingredients']['coffee']
-    if coffee_choice != 'espresso':
-        resources['milk'] -= MENU[coffee_choice]['ingredients']['milk']
+def check_for_remaining_change(amt_paid, coffee):
+    if amt_paid > coffee['cost']:
+        print(f"Here is your remaining balance: ${round(amt_paid - coffee['cost'],2)}")
 
 
 def coffee_machine():
-    on = True
-    while on:
-        choice = input("What would you like? Type 'espresso', 'latte', 'cappuccino': ")
-        if choice == 'report':
-            report()
-            coffee_machine()
-        elif choice == 'off':
-            print("Turning off.")
-            on = False
-        elif check_resources(choice):
-            print("Please insert coins.")
-            quarters = int(input("How many quarters?: "))
-            dimes = int(input("How many dimes?: "))
-            nickles = int(input("How many nickles?: "))
-            pennies = int(input("How many pennies?: "))
-            if process_coins(quarters, dimes, nickles, pennies) == MENU[choice]['cost']:
-                deduct_resources(choice)
-                money['value'] += MENU[choice]['cost']
-                print(f"Here is your {choice} ☕. Have a good one!")
-            elif process_coins(quarters, dimes, nickles, pennies) > MENU[choice]['cost']:
-                deduct_resources(choice)
-                money['value'] += MENU[choice]['cost']
-                change = process_coins(quarters, dimes, nickles, pennies) - MENU[choice]['cost']
-                change = round(change, 2)
-                print(f"Here is ${change} in change.")
-                print(f"Here is your {choice} ☕. Have a good one!")
-            else:
-                print(f"Not enough money, ${(process_coins(quarters, dimes, nickles, pennies))} refunded.")
+    while True:
+        user_input = input("What would you like to drink? \n Type 'espresso' -> $1.5 \n 'latte' -> $2.5 \n 'cappuccino' -> $3.0\n")
 
+        if user_input == 'off':
+            print("Turning off the Coffee Machine")
+            break
+
+        elif user_input == 'report':
+            get_report()
+
+        elif user_input in MENU:
+            coffee = MENU[user_input]
+            check_resources(user_input, coffee, resources)
+            amt_paid = request_money()
+            validate_cost(coffee, amt_paid)
+            dispense_coffee(user_input, coffee, resources)
+            update_profit(coffee)
+            check_for_remaining_change(amt_paid, coffee)
+            get_report()
 
 coffee_machine()
+    
+
+    
